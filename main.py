35da32a0596ed0a7fce0474e6d5ae69aba6dfe50
paulbui <buipaul1@msu.edu>
2025-11-10 21:27:14 -0500

    Testing more parts
    -Learned to extract href link information from the inputted job linnk
    - Trying to take the extracted link information and make requests to those links and extract job desc info from it. Not working perhaps in part that the requests are in a for loop, repeated call requests could be banning me from making these requests to linked in as I have no proxy server to evade being classified as a bot to the linkedin servers

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 
 
 #link to testing here
 
-#https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?trk=guest_homepage-basic_guest_nav_menu_jobs&start=100
-
-link_get = 'https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?trk=guest_homepage-basic_guest_nav_menu_jobs&start=100'
+link_get = 'https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=Python&location=United%2BStates&geoId=103644278&trk=public_jobs_jobs-search-bar_search-submit&currentJobId=4339591541&position=3&pageNum=0&start=25'
 
 
 testing = requests.get(link_get)
@@ -18,16 +17,60 @@ testing = requests.get(link_get)
 soup = BeautifulSoup(testing.text, 'lxml')
 
 ### Writing file to have a prettier html file
-testing_file = "testing_file.txt"
-with open(testing_file, "w") as file:
+job_list_html = "joblist.txt"
+job_description_html = "JobDescription.txt"
+with open(job_list_html, "w") as file:
     file.write(soup.prettify())
 
-print(soup.find_all('span', class_ = "sr-only"))
+def has_href(href):
+    return href.has_attr('href)')
 
-print("-----------")
+listed_span = soup.find_all(href=re.compile("https://www.linkedin.com"))
+print(listed_span)
+
+for i in listed_span:
+    print("----------- Listed Elements from Website --------------")
+    #attrs grabs the attribute/attribute values from the tag
+    #get grabs the specific value from the dictionary
+    job_link = i.attrs.get('href')
+    grabbing_job_website = requests.get(job_link)
+    converting_job_link = BeautifulSoup(grabbing_job_website.text, 'lxml')
+
+    with open(job_description_html, "w") as file:
+        file.write(converting_job_link.prettify())
+
+    job_description = converting_job_link.find_all('a', class_ = 'core-section-container')
+
+    print("xxxxxx Job Link xxxxxx")
+    print(i.attrs.get('href'))
+
+    print("xxxxxx Grabbing the job description") 
+    print(job_description)
+
+
+
+
+print("------ Testing the contents of span below -----")
 
 test = (soup.span.contents[0].strip())
 test_2 = (soup.span.contents)
 print(test)
 print(test_2)
+
+
+
+
+class LinkedInScrapper:
+
+    #create the various lists to store the information from the website
+    def __init__(self):
+        #initialize a dictionary for storing job information that'll be uploaded to the database
+        # we want a dictionary of a string key, and an array of strings with the information of that job
+        # {string, [string]}
+        job_information = {}
+
+    
+
+
+        
 
