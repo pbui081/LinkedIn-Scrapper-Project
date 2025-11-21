87f20b503702276a0e6d055d10e89e74fa88d9c6
Paul D Bui <buipaul1@msu.edu>
2025-11-21 14:10:40 -0500

    environment and extra helper functions
    created environment on desktop and added gitignore to ignore environment

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from flask import Flask, render_template
 link_get = 'https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=Python&location=United%2BStates&geoId=103644278&trk=public_jobs_jobs-search-bar_search-submit&currentJobId=4339591541&position=3&pageNum=0&start=25'
 
 
-get_request = requests.get(link_get)
 
 
 """
@@ -30,7 +29,7 @@ def link_to_soup(link, file_to_write) -> BeautifulSoup:
         return soup_object
 
     except:
-        print("Error Occurred")
+        print("Error Occurred: link_to_soup from: ", file_to_write)
         exit(-1)
         pass
 
@@ -46,16 +45,50 @@ Expected reutrn: string type
 def grab_job_desc(soup_object : BeautifulSoup) -> str:
     try:
         job_description = soup_object.find(class_ = "show-more-less-html__markup")
-        return job_description.get_text(" ", strip=True)
+        if(job_description is not None):
+            return job_description.get_text(" ", strip=True)
+        else:
+            print("Error has occured looking up element")
+            exit(-1)
     except:
         print("Error has occured while grabbing job description")
         exit(-1)
-    pass
+    pass    
+
+def strip_tag(list_package: list) -> list:
+    for i in range(len(list_package)):
+        list_package[i] = list_package[i].get_text(strip=True)
+    return list_package
+
+def grab_job_points(soup_object : BeautifulSoup) :
+    try:
+        job_point = soup_object.find(class_="show-more-less-html__markup")
+        
+        if job_point:
+            find_bullet_points = job_point.find_all("li")
+            if find_bullet_points:
+                
+                return strip_tag(find_bullet_points)
+            
+        else:
+            
+            print("Error has occured looking up element")
+            exit(-1)
+            
+    except:
+        print("error has occured grabbing job points in file")
+        exit(-1)
+        pass
 
 def grab_company_name(soup_object : BeautifulSoup) -> str:
     try:
         company_name = soup_object.find(class_ = "topcard__org-name-link topcard__flavor--black-link")
-        return company_name.get_text(strip=True)
+        if(company_name is not None):
+            return company_name.get_text(strip=True)
+        else:
+            print("Error has occured looking up element")
+            exit(-1)  
+        
     except:
         print("Error has occured grabbing Company Name")
         exit(-1)
@@ -94,13 +127,21 @@ converting_job_link = BeautifulSoup(grabbing_job_website.text, 'lxml')
 first_soup =  link_to_soup(first_job, "first_job_html.txt")
 second_soup = link_to_soup(second_job, "second_job_html.txt")
 
+
+
+
 ######## This grabs the job description below 
 
 print("__________________________________________________________")
 #job 1
 
+job_point = first_soup.find("ul", class_="show-more-less-html__markup")
+if job_point:
+    job_point.find_all("li")
+
 print(grab_company_name(first_soup))
-print(grab_job_desc(first_soup))
+print(grab_job_points(first_soup))
+
 
 print("__________________________________________________________")
 
@@ -108,7 +149,7 @@ print("__________________________________________________________")
 #job 2 
 print(grab_company_name(second_soup))
 
-print(grab_job_desc(second_soup))
+
 
 
     
@@ -140,7 +181,7 @@ class db:
         self.cursor.execute("""
         CREATE TABLE IF NOT EXISTS jobs (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-                title TEXT
+                title TEXT,
                 company TEXT,      
                 job_desc, TEXT,
                 location TEXT,
@@ -148,7 +189,7 @@ class db:
             )
 
         """)
-        self.cursor.commit()
+        self.db.commit()
 
     def insert_information(self, company : str, job_desc : str, location : str, date_added, str):
         self.cursor.execute("""
@@ -159,8 +200,8 @@ class db:
     #This is going to delete the database in order to refresh the table with new job information
     def refresh_table(self):
         self.cursor.execute("DROP TABLE IF EXISTS jobs")
-        conn.commit()
+        self.db.commit()
 
-        conn.close()
+        self.db.close()
         
 
